de8f24a6dee0948179071458ca839017eef954a4
Mikhail Pak <mikhail.pak@tum.de>
2016-10-11 11:04:15 +0200

    Added names highlighting

--- markdown.go
+++ markdown.go
@@ -2,10 +2,18 @@ package clerk
 
 import (
 	"io/ioutil"
+	"regexp"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
 )
+
+// \b - word boundary
+// \@ - as literal
+// \w+ - at least one word character, greedily
+var userRegexp = regexp.MustCompile(`(\W)(\@\w+)`)
+
+const colorHighlighting = `$1<b><font color="#ff9933">$2</font></b>`
 
 func MarkdownToHTML(filename string) ([]byte, error) {
 	data, err := ioutil.ReadFile(filename)
@@ -16,5 +24,11 @@ func MarkdownToHTML(filename string) ([]byte, error) {
 	unsafe := blackfriday.MarkdownCommon(data)
 	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 
+	html = []byte(highlightUsers(string(html)))
+
 	return html, nil
+}
+
+func highlightUsers(html string) string {
+	return userRegexp.ReplaceAllString(html, colorHighlighting)
 }
